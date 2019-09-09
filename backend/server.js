f0679b700a2bf10d194239280e8f260019d0b7c8
Tyler Allen <tyler.allen@akqa.com>
2019-09-08 22:41:21 -0700

    add middleware and POST request handler

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,21 @@ const app = express();
 const cors = require('cors');
 const PORT = 4000;
 
+const allowCrossDomain = function (req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header("Access-Control-Allow-Credentials", "true");
+	res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
+	res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+	next();
+};
+
+//middleware
+app.use(express.static('build'));
+app.set('views', './build');
+app.use(allowCrossDomain);
+app.use(cors());
+app.use(express.json());
+
 const { Client } = require('espn-fantasy-football-api/node');
 const myEmail = require('./email.js');
 const matchUtils = require('./utilities').Match;
@@ -10,11 +25,11 @@ const myClient = new Client({ leagueId: 366210 });
 
 myClient.setCookies({ espnS2: 'AEBxj59LhZR9gGHOlmjtmtwq29SGQJfSMCTgBIZR2a4tpSS3oBKdN9Pv%2B%2BmEVT7wdevZjt51N9iHUkM%2B%2FXAIBOemDz%2BNHKRCRmaDYZQpaO5Bgq9tZLus6vGri6kLGIFCd7T2B1Zb5l7nUhj5gCKz2juRYhUyDOCFN85z%2BsHZGOLsadDirS4mrnUE5EHCYcK25ZvaNs1hLWynHKTpgatQ9%2B6A9vrE7HItdQkND9WeSbatEcPrXlceuI8JqNtGI6DbbO5ZIPzimUQ1xbQ1Bw7y1dupadT5if6MMBWNAQncpqslEg%3D%3D', SWID: '73675E39-CDD8-49A3-8421-D94C4C7105D0' });
 
-let data;
+let data = {};
 
 myClient.getBoxscoreForWeek({ seasonId: 2018, scoringPeriodId: 5, matchupPeriodId: 5 }).then((boxscores) => {
-	data = boxscores;
-	const scores = matchUtils.sortScores(data);
+	data = { boxscores };
+	const scores = matchUtils.sortScores(data.boxscores);
 
 	const lowScoreTest = matchUtils.lowestTeamScore(boxscores);
 	const highScoreTest = matchUtils.highestTeamScore(boxscores);
@@ -30,12 +45,19 @@ myClient.getBoxscoreForWeek({ seasonId: 2018, scoringPeriodId: 5, matchupPeriodI
 	}
 });
 
+app.post('/', (req, res) => {
+	const response = req.body;
+	console.log(response.seasonID);
+	console.log(response.scoringPeriod);
+	console.log(response.leagueID);
+	// console.log(req.body) //undefined
+	res.end("Success")
+})
+
 app.get('/', function (req, res) {
 	res.status(200).send(data);
 });
 
-app.use(cors());
-app.use(express.json());
 
 app.listen(PORT, function() {
 	console.log("Server is running on Port: " + PORT);
